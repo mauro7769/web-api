6bb381d146361a5d66add2f0ae9e5f417f9756f3
Mauro <mau.pinop@alumnos.duoc.cl>
2018-01-22 00:41:39 -0300

    commit inicial

---
+++ ClientesWebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ClientesWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
